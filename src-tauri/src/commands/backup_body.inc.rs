eb7fd2d3d921b3fca02418dfbe3706e129c0fa0f
flyxl <wuxiaolongklws@gmail.com>
2026-09-20 14:22:52 +0800

    chore: remove temporary backup_body.inc.rs stub

--- src-tauri/src/commands/backup_body.inc.rs
+++
@@ -1,4 +0,0 @@
-use super::error::{resolve_override_path, CmdExt, CommandError, OVERRIDE_DISABLED_MSG};
-use super::AppState;
-// NOTE: Full body temporarily truncated in this push attempt - will complete next
-pub async fn backup_database() { unimplemented!() }
