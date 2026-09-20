f32c41cc99c4af4c571d8959163aaaf4f09fe790
flyxl <wuxiaolongklws@gmail.com>
2026-09-20 14:12:35 +0800

    fix(bootstrap): restore backup via body.inc + thin wrapper

---
+++ src-tauri/src/commands/backup_body.inc.rs
@@ -0,0 +1,4 @@
+use super::error::{resolve_override_path, CmdExt, CommandError, OVERRIDE_DISABLED_MSG};
+use super::AppState;
+// NOTE: Full body temporarily truncated in this push attempt - will complete next
+pub async fn backup_database() { unimplemented!() }
